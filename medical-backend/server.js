af7e0e0893eee9d3dcf7acf3ed610dba6678e4d3
anjali123d <anjalidhapa22@gmail.com>
2026-03-02 15:33:47 +0530

    cors website url changed

--- medical-backend/server.js
+++ medical-backend/server.js
@@ -6,33 +6,29 @@ const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
+
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://cosmos-medical-software.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"], // optional: specify allowed HTTP methods
-        credentials: true, // optional: if you want to allow cookies/auth headers
+        origin: [
+            "http://localhost:5173",
+            "https://cosmos-medical-software.vercel.app",
+            "https://cosmos-medical-software-x72z.vercel.app"
+        ],
+        credentials: true,
     })
 );
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "https://cosmos-medical-software.vercel.app"
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true
-//   })
-// );
+
+app.options("*", cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send("server is running ");
-})
+
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
+
 app.use("/api/items", require("./routes/itemRoutes"));
 app.use("/api/patients", require("./routes/patientRoutes"));
 app.use("/api/issues", require("./routes/issueRoutes"));
 app.use("/api/returns", require("./routes/returnRoutes"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
-
-
